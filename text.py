0a72ceb48fd488f267665ddd6b5e70ccd77e15b2
wjc2008 <44060208+wjc2008@users.noreply.github.com>
2019-10-08 08:19:39 +0800

    Update text.py

--- text.py
+++ text.py
@@ -1,15 +1,13 @@
-print("这是一个测试git的文件")
-a = "测试成功"
-print(a)
-s_a = input("请输入一个数：")
-a = int(s_a)
-s_b = input("请再输入一个数：")
-b = int(s_b)
-c = a ** b
-print(c)
-<<<<<<< HEAD
-print("安装git.exe成功")
-print("能自动保存吗？看看吧")
-=======
-print("安装git.exe成功")
->>>>>>> dcee658ff8876dc86a1e53ab174697360e4ddad5
+import pygame
+import sys
+
+pygame.init()  # 初始化pygame
+size = width, height = 320, 240  # 设置窗口大小
+screen = pygame.display.set_mode(size)  # 显示窗口
+
+while True:  # 死循环确保窗口一直显示
+    for event in pygame.event.get():  # 遍历所有事件
+        if event.type == pygame.QUIT:  # 如果单击关闭窗口，则退出
+            sys.exit()
+
+pygame.quit()  # 退出pygame
